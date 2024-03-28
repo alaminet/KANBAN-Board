45f4da2e49cdcc91b21284bacddd737837b76080
Al Amin ET <alaminet.bd@gmail.com>
2024-03-28 23:31:14 +0600

    mention-optione-added

--- kanban-board/src/components/Mention.jsx
+++ kanban-board/src/components/Mention.jsx
@@ -1,55 +1,45 @@
 import React, { useCallback, useRef, useState } from "react";
-
-import { Mentions } from "antd";
+import { Button, Mentions } from "antd";
 
 const Mention = () => {
-  const [loading, setLoading] = useState(false);
-  const [users, setUsers] = useState([]);
-  const ref = useRef();
-  const loadGithubUsers = (key) => {
-    if (!key) {
-      setUsers([]);
-      return;
-    }
-    fetch(`https://api.github.com/search/users?q=${key}`)
-      .then((res) => res.json())
-      .then(({ items = [] }) => {
-        if (ref.current !== key) return;
-        setLoading(false);
-        setUsers(items.slice(0, 10));
-      });
+  const [mention, setMention] = useState([]);
+
+  const onChange = (value) => {
+    console.log("Change:", value);
   };
-  const debounceLoadGithubUsers = useCallback(
-    debounce(loadGithubUsers, 800),
-    []
-  );
-  const onSearch = (search) => {
-    console.log("Search:", search);
-    ref.current = search;
-    setLoading(!!search);
-    setUsers([]);
-    debounceLoadGithubUsers(search);
+  const onSelect = (option) => {
+    setMention([...mention, option]);
   };
+  console.log(mention);
   return (
     <>
       <Mentions
         style={{
-          width: "100%",
+          width: "100px",
         }}
-        loading={loading}
-        onSearch={onSearch}
-        options={users.map(({ login, avatar_url: avatar }) => ({
-          key: login,
-          value: login,
-          className: "antd-demo-dynamic-option",
-          label: (
-            <>
-              <img src={avatar} alt={login} />
-              <span>{login}</span>
-            </>
-          ),
-        }))}
+        onChange={onChange}
+        onSelect={onSelect}
+        defaultValue="@"
+        options={[
+          {
+            value: "afc163",
+            label: "afc163",
+          },
+          {
+            value: "zombieJ",
+            label: "zombieJ",
+          },
+          {
+            value: "yesmeck",
+            label: "yesmeck",
+          },
+        ]}
       />
+      {mention.map((item, i) => (
+        <Button key={i} type="primary">
+          {item.value}
+        </Button>
+      ))}
     </>
   );
 };
